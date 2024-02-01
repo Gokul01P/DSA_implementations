799d4e875095213d31ba21161ff9d59b2a468013
Gokul01P <gokulkannan06012003@gmail.com>
2024-02-01 08:22:39 +0530

    InsertionSort added

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+import java.util.Arrays;
+
+import static Sorting.SelectionSort.swap;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr={3,-1,0,21,8,-23};
+
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j > 0 ; j--) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+}
